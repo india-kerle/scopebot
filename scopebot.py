471dc3b422cc39a5e4d742051ce6c4d0daac5b5a
India Kerle <india.kerle@nesta.org.uk>
2021-12-16 11:32:03 +0000

    debugged scope counter function

--- scopebot.py
+++ scopebot.py
@@ -16,43 +16,45 @@ def get_slack_history(client):
     
     channel_ids = [channel['id'] for channel in client.conversations_list()['channels'] if channel['is_private'] == False]
 
-    messages = []
+
     time_stamp = datetime.datetime.now(tz=None)
     last_week = time_stamp - datetime.timedelta(days=7)
-    
+
+    channel_messages = []
     for channel_id in channel_ids:
         try:
-            response = client.conversations_history(
-                channel=channel_id,
-                latest=time.mktime(time_stamp.timetuple()),
-                oldest=time.mktime(last_week.timetuple()),
-                inclusive=True
-            )
-            if response['messages']:
-                messages.extend(text['text'] for text in response['messages'])
-
+            rs = client.conversations_history(
+                    channel=channel_id,
+                    latest=time.mktime(time_stamp.timetuple()),
+                    oldest=time.mktime(last_week.timetuple()),
+                    inclusive=True)
+            if rs['messages']:
+                for channel_message in rs['messages']:
+                    if 'bot_id' not in channel_message.keys():
+                        channel_messages.append(channel_message['text'])
             else:
                 print('no messages!')
-
-            return messages
-
+    
         except SlackApiError as e:
             assert e.response["ok"] is False
             assert e.response["error"]
             print(f"Got an error: {e.response['error']}")
+    
+    return channel_messages
 
 def scope_counter(channel_text):
     '''counts the number of times words begin with 'scop' across channel messages.'''
-   
-    scope_counter = 0    
+    
+    scope_counts = []
     if channel_text is not None:
         for text in channel_text:
             text = text.translate(str.maketrans('', '', string.punctuation))
             text = text.lower()
-            if text.startswith('scop'):
-                scope_counter += 1
-                
-        return f'We said some version of the word "scope" {scope_counter} times this week! So the scoping talk rages on...'
+            for t in text.split(' '):
+                if t.startswith('scop'):
+                    scope_counts.append(t)
+
+        return f'We said some version of the word "scope" {len(scope_counts)} times this week! So the scoping talk rages on...'
     
     else:
         print('no messages!')
@@ -64,7 +66,6 @@ def scopebot(client, channel_name='#test-bot'):
         response = client.chat_postMessage(
             channel=channel_name,
             text=scope_counter(slack_messages),
-            icon_emoji= ':chart_with_upwards_trend:'
         )
         
     except SlackApiError as e:
